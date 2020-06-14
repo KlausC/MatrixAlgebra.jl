cf8fe2a8e094b6020a0aad0b4a2aed16422868d7
Klaus Crusius <klaus.crusius@web.de>
2020-06-14 11:31:09 +0200

    support :U and :L

--- src/tridiagonalize.jl
+++ src/tridiagonalize.jl
@@ -11,51 +11,91 @@ For general matrices only `d` superdiagonals are processed.
 Works efficiently for `BandedMatrices`.
 """
 function tridiag_real(A::AbstractMatrix, d::Integer)
-    td = make_tridiag!(copy(A), d)
-    SymTridiagonal(Symmetric(real(td), :L))
+    n = size(A, 1)
+    B = copybands(A, d)
+    td = make_tridiag!(B, Val('L'), d)
+    dv = [real(B[i,i]) for i in 1:n]
+    ev = [real(B[i,i-1]) for i = 2:n]
+    SymTridiagonal(dv, ev)
 end
 
-function make_tridiag!(A, d)
-    n = size(A, 1)
+function make_tridiag!(A, ::Val{uplo}, d) where uplo
+    m, n = size(A)
+    m == n || throw(DimensionMismatch("matrix is not square: dimensions are $((m, n))"))
+    0 < d < n || throw(ArgumentError("number of superdiagonals $d not in 1:$(n-1)"))
+
     for bm = d:-1:2
         for k = 1:n-bm
             kp = k
-            apiv = A[bm+kp, kp]
-            #println("first piv A[$(bm+kp),$kp] = $apiv")
+            apiv = uplo == 'L' ? A[bm+kp, kp] : A[kp, bm+kp]
             for i = bm+k-1:bm:n-1
-                b = A[i,kp]
+                b = uplo == 'L' ? A[i,kp] : A[kp, i]
                 #c2, s2, r2 = LinearAlgebra.givensAlgorithm(b, apiv)
                 # f = r2' / abs(r2); (c, s, r) = (c2', s2', r2') .* f'
                 r = hypot(b, apiv)
                 c = b / r
                 s = apiv / r
-                upx = (A[i,i] + A[i+1,i+1]) / 2
-                umx = (A[i,i] - A[i+1,i+1]) / 2
-                A[i,i] = umx
-                A[i,kp] = r
-                A[i+1,kp] = 0
+                u, v = A[i,i], A[i+1,i+1]
+                upx = (u + v) / 2
+                A[i,i] = (u - v) / 2
+                if uplo == 'L'
+                    A[i,kp], A[i+1,kp] = r, 0
+                else
+                    A[kp,i], A[kp,i+1] = r, 0
+                end
                 for j = kp+1:i
-                    A[i,j], A[i+1,j] = A[i,j] * c' + A[i+1,j] * s', -A[i,j] * s + A[i+1,j] * c
+                    if uplo == 'L'
+                        u, v = A[i,j], A[i+1,j]
+                        A[i,j], A[i+1,j] = u * c' + v * s', -u * s + v * c
+                    else
+                        u, v = A[j,i], A[j,i+1]
+                        A[j,i], A[j,i+1] = u * c' + v * s', -u * s + v * c
+                    end
                 end
                 A[i+1,i+1] = -A[i,i]'
                 ip = i + bm
                 for j = i+1:min(ip, n)
-                    A[j,i], A[j,i+1] = A[j,i] * c + A[j,i+1] * s, -A[j,i] * s' + A[j,i+1] * c'
+                    if uplo == 'L'
+                        u, v = A[j,i], A[j,i+1]
+                        A[j,i], A[j,i+1] = u * c + v * s, -u * s' + v * c'
+                    else
+                        u, v = A[i,j], A[i+1,j]
+                        A[i,j], A[i+1,j] = u * c + v * s, -u * s' + v * c'
+                    end
                 end
                 r = real(A[i+1,i+1])
                 A[i,i] = upx - r
                 A[i+1,i+1] = upx + r
                 if ip < n
-                    apiv, A[ip+1,i+1] = A[ip+1,i+1] * s, A[ip+1,i+1] * c'
-                    #A[ip+1,i] = apiv
-                    #println("piv A[$(ip+1),$(i+1)] = $apiv")
+                    if uplo == 'L'
+                        v = A[ip+1,i+1]
+                        A[ip+1,i+1] = v * c'
+                    else
+                        v = A[i+1,ip+1]
+                        A[i+1,ip+1] = v * c'
+                    end
+                    apiv = v * s
                 end
-                #println("bm = $bm i = $i k = $k ip = $ip")
-                #display(A)
                 kp = i
             end
         end
     end
-    A[n,n-1] = abs(A[n,n-1])
+    if uplo == 'L'
+        A[n,n-1] = abs(A[n,n-1])
+    else
+        A[n-1,n] = abs(A[n-1,n])
+    end
     A
+end
+
+copybands(A, d) = copybands!(zero(A), A, d)
+
+function copybands!(B::AbstractMatrix, A::AbstractMatrix, d::Integer)
+    m, n = size(A)
+    for j = 1:n
+        for i = j:min(j+d,m)
+            B[i,j] = A[i,j]
+        end
+    end
+    B
 end
